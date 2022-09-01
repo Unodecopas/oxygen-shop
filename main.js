06f412c0443461a64f757f6f5804661ec4e47ae7
Jesus Gallardo <gallardomoralesjesus@gmail.com>
2022-09-01 19:56:30 +0200

    feat: add validation length for name and regex for email

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const percentScroller = document.querySelector(".percentage__scroller");
 const modal = document.querySelector('.modal')
 const closeModal = document.querySelector('.close__modal')
 const subscribe = document.querySelector('.subscribe')
+const inputName = document.querySelector('#name')
+const inputEmail = document.querySelector('#email')
 
 //BUTTON FOR BACK TO TOP
 const toTop = () => {
@@ -58,3 +60,30 @@ modal.addEventListener("click", (e) => {
     modal.style.display = "none";
   }
 });
+
+//VALIDACION FORMULARIO
+const validateNameLength = () => {
+  if (inputName.value.length > 2 && inputName.value.length <= 100){
+    inputName.setAttribute("style", "  border-bottom: 1px solid var(--grey)");
+  }else {
+    inputName.setAttribute(
+        "style",
+        "border:1px solid red ; padding: 7px ; border-radius: 10px"
+      )
+  }
+}
+const validateEmail = () => {
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!inputEmail.value.match(regex)){
+    inputEmail.setAttribute(
+        "style",
+        "border:1px solid red ; padding: 7px ; border-radius: 10px"
+      );
+  }else {
+    inputEmail.setAttribute("style", "  border-bottom: 1px solid var(--grey)");
+
+  }
+}
+
+inputName.addEventListener('input', validateNameLength);
+inputEmail.addEventListener("input", validateEmail);
