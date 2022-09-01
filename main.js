d92fcfe986594470964b972a593c5bb6d13fd27d
Jesus Gallardo <gallardomoralesjesus@gmail.com>
2022-09-01 11:10:08 +0200

    feat: add logic for modal

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const btnToTop = document.querySelector('.scroll__up')
 const percentScroller = document.querySelector(".percentage__scroller");
+const modal = document.querySelector('.modal')
+const closeModal = document.querySelector('.close__modal')
+const subscribe = document.querySelector('.subscribe')
 
-
+//BUTTON FOR BACK TO TOP
 const toTop = () => {
   document.documentElement.scrollTo({
     behavior: "smooth",
@@ -12,6 +15,7 @@ const backToTop = () => setTimeout(toTop, 200)
 btnToTop.addEventListener('click', backToTop)
 
 
+//SCROLL PERCENTAGE
 let scrollPercent;
 document.addEventListener("scroll", () => {
   const pixels = scrollY;
@@ -19,5 +23,38 @@ document.addEventListener("scroll", () => {
   const winHeight = innerHeight;
   scrollPercent = (pixels / (docHeight - winHeight)) * 100;
   percentScroller.style.width = scrollPercent + "%";
-  console.log(scrollPercent)
+})
+
+//MODAL
+let modalShow = true
+setTimeout(() => {
+  if (localStorage.getItem("modalShown") !== "1") {
+    modal.style.display = "block";
+    localStorage.setItem("modalShown", "1");
+  }
+}, 5000);
+
+const showModal = () => {
+  modalShow = false
+}
+
+document.addEventListener("evt", showModal);
+
+document.addEventListener("scroll", () => {
+  if (localStorage.getItem("modalShown") !== "1" && scrollPercent >= 25) {
+    modal.style.display = "block";
+    localStorage.setItem("modalShown", "1");
+  }
+});
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+subscribe.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target === e.currentTarget) {
+    modal.style.display = "none";
+  }
 });
