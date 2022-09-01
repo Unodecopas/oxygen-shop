d95fee6c89e54cae7cc3ef0c10b2486b913dc15c
Jesus Gallardo <gallardomoralesjesus@gmail.com>
2022-09-01 10:48:47 +0200

    feat: add percetage scroller at top

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const btnToTop = document.querySelector('.scroll__up')
+const percentScroller = document.querySelector(".percentage__scroller");
 
 
 const toTop = () => {
@@ -10,3 +11,13 @@ const toTop = () => {
 const backToTop = () => setTimeout(toTop, 200)
 btnToTop.addEventListener('click', backToTop)
 
+
+let scrollPercent;
+document.addEventListener("scroll", () => {
+  const pixels = scrollY;
+  const docHeight = document.body.offsetHeight;
+  const winHeight = innerHeight;
+  scrollPercent = (pixels / (docHeight - winHeight)) * 100;
+  percentScroller.style.width = scrollPercent + "%";
+  console.log(scrollPercent)
+});
