01530575d86af9cc4ac1f0bf7cf2c1720be89dc5
Jesus Gallardo <gallardomoralesjesus@gmail.com>
2022-09-02 09:57:06 +0200

    feat: add slider, menu and fetch

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ const closeModal = document.querySelector('.close__modal')
 const subscribe = document.querySelector('.subscribe')
 const inputName = document.querySelector('#name')
 const inputEmail = document.querySelector('#email')
+const btnSubmit = document.querySelector('#btn__submit')
+const btnHeader = document.querySelector('.header__button')
+const navbar = document.querySelector('.navbar')
 
 //BUTTON FOR BACK TO TOP
 const toTop = () => {
@@ -87,3 +90,106 @@ const validateEmail = () => {
 
 inputName.addEventListener('input', validateNameLength);
 inputEmail.addEventListener("input", validateEmail);
+
+
+//FETCH
+
+const submitForm = async (e) => {
+  e.preventDefault()
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts",{
+      method: 'POST',
+      headers: {
+      "Content-type": "application/json"
+      },
+      body: JSON.stringify({name: inputName.value, email: inputEmail.value})
+    })
+    if (res.ok){
+      const data = await res.json()
+      console.log(data)
+    }
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+btnSubmit.addEventListener('click', submitForm)
+
+//SLIDER
+
+class SuperSlider {
+  constructor(id) {
+    this.container = document.getElementById(id);
+    this.images = this.container.getElementsByClassName("slide__img");
+    this.activeImg = this.images[0];
+    this.activeIndex = 0;
+    this.arrow = document.createElement("button");
+    this.arrowRight = document.createElement("button");
+    this.arrow.classList.add("slide__btn");
+    this.arrowRight.classList.add("slide__btn--right", "slide__btn");
+    this.arrow.textContent = "❮";
+    this.arrowRight.textContent = "❯";
+    this.dotsContainer = document.createElement("div");
+    this.dotsContainer.classList.add("dotsContainer");
+
+    this.container.appendChild(this.arrow);
+    this.container.appendChild(this.arrowRight);
+    this.container.appendChild(this.dotsContainer);
+    this.dots();
+    this.dots[0].classList.add("dot--active");
+    this.activeDot = this.dots[0];
+
+    setInterval(() => {
+      this.nextImg();
+    }, 2000);
+    this.arrow.addEventListener("click", () => {
+      this.prevImg();
+    });
+    this.arrowRight.addEventListener("click", () => {
+      this.nextImg();
+    });
+  }
+
+  dots() {
+    this.dots = [];
+    for (let i = 0; i < this.images.length; i++) {
+      const dot = document.createElement("button");
+      dot.classList.add("dot");
+      this.dotsContainer.appendChild(dot);
+      this.dots.push(dot);
+    }
+  }
+  nextImg() {
+    if (this.activeIndex >= this.images.length - 1) {
+      this.showImg(0);
+    } else {
+      this.showImg(this.activeIndex + 1);
+    }
+  }
+
+  prevImg() {
+    if (this.activeIndex === 0) {
+      this.showImg(this.images.length - 1);
+    } else {
+      this.showImg(this.activeIndex - 1);
+    }
+  }
+  showImg(index) {
+    this.activeImg.classList.remove("slide__img--active");
+    this.images[index].classList.add("slide__img--active");
+    this.activeDot.classList.remove("dot--active");
+    this.dots[index].classList.add("dot--active");
+    this.activeDot = this.dots[index];
+    this.activeImg = this.images[index];
+    this.activeIndex = index;
+  }
+}
+
+const slider = new SuperSlider("slider");
+
+
+//MENU
+const showMenu = () => {
+  navbar.classList.toggle('nav--visible')
+}
+btnHeader.addEventListener('click', showMenu)
